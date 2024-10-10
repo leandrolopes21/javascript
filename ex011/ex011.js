750239038f0154535e38b7cf598ab10ac99d3d04
Leandro Lopes <leandrolopeslive21@hotmail.com>
2024-10-09 21:36:06 -0300

    Arquivos atualizados

---
+++ ex011/ex011.js
@@ -0,0 +1,7 @@
+var vel = 50;
+
+if (vel > 60) {
+    console.log("Você está acima da velocidade permitida!");
+}
+
+console.log("Tudo certo!");
---
+++ ex011/ex012.js
@@ -0,0 +1,7 @@
+var pais = 'Irã';
+
+if (pais == 'Brasil') {
+    console.log("Você é brasileiro");
+} else {
+    console.log("Você é estrangeiro");
+}
