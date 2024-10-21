0390692133b4574072e9596a4c694d8b23d4331b
Leandro Lopes <leandrolopeslive21@hotmail.com>
2024-10-21 20:11:53 -0300

    Teste para aplicação de contador

---
+++ repeticao/repeticao.js
@@ -0,0 +1,22 @@
+/*
+Exemplo de estrutura de repetição while(condição), que primeiro testa a condição e depois executa
+*/
+
+
+var i = 0
+
+while (i <= 30000) { // Leia enquanto a variável i for menor ou igual a 500000
+    console.log(`Número ${i}`) //Apresenta na tela o valor de i enquanto a condição acima for verdadeira
+    i++ // É o mesmo que i = i + 1
+}
+
+/*
+Exemplo de estrutura de repetição do {} while(condição) que primeiro executa a condição
+
+var i = 0
+
+do {
+    console.log(`Número ${i}`) //Apresenta na tela o valor de i enquanto a condição abaixo for verdadeira
+    i++ // É o mesmo que i = i + 1
+} while (i <= 500000); // Leia enquanto a variável i for menor ou igual a 500000
+ */
