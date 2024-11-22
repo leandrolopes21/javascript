3899709a11656872ac4448bae874eb8d3047716d
LeandroLopes21 <leandrolopeslive21@hotmail.com>
2024-11-21 21:39:04 -0300

    Atualização
    Aula 4 Alura

--- Alura/imersao back end/instalike-back/src/models/postModel.js
+++ Alura/imersao back end/instalike-back/src/models/postModel.js
@@ -3,11 +3,17 @@ import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para obter todos os posts do banco de dados
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Seleciona o banco de dados "imersao-instabytes" na conexão estabelecida
     const db = conexao.db("imersao-instabytes");
     // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
     // Executa uma consulta para encontrar todos os documentos na coleção e retorna os resultados como um array
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 }
--- Alura/imersao back end/instalike-back/src/routes/postsRoutes.js
+++ Alura/imersao back end/instalike-back/src/routes/postsRoutes.js
@@ -1,11 +1,28 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({dest:"./uploads", storage})
+// linux ou mac
+// const upload = multer({dest:"./uploads"})
 
 const routes = (app) => {
-    // Habilita o middleware para analisar o corpo das requisições JSON
+    // Permite que o servidor interprete requisições com corpo no formato JSON
     app.use(express.json());
-    // Define uma rota GET para a URL /posts
+    // Rota para buscar todos os posts
     app.get("/posts", listarPosts);
+    // Rota para criar um post
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
