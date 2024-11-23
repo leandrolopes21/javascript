bc5e868161fcdae059ee2456cfbe12bec4b377e6
LeandroLopes21 <leandrolopeslive21@hotmail.com>
2024-11-22 21:38:09 -0300

    Alura
    Aula 5 Alura

--- Alura/imersao back end/instalike-back/server.js
+++ Alura/imersao back end/instalike-back/server.js
@@ -2,7 +2,10 @@ import express from "express"; // Importa o framework Express para criar a aplic
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express(); // Cria uma instância do Express, iniciando a aplicação
-routes(app)
+
+app.use(express.static("uploads"));
+
+routes(app);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver ouvindo
 app.listen(3000, () => {
--- Alura/imersao back end/instalike-back/src/routes/postsRoutes.js
+++ Alura/imersao back end/instalike-back/src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,11 +24,15 @@ const upload = multer({dest:"./uploads", storage})
 const routes = (app) => {
     // Permite que o servidor interprete requisições com corpo no formato JSON
     app.use(express.json());
+    app.use(cors(corsOptions))
     // Rota para buscar todos os posts
     app.get("/posts", listarPosts);
-    // Rota para criar um post
+    // Rota para criar um novo post
     app.post("/posts", postarNovoPost);
+    // Rota para upload de imagens (assumindo uma única imagem chamada "imagem")
     app.post("/upload", upload.single("imagem"), uploadImagem);
-}
+
+    app.put("/upload/:id", atualizarNovoPost);
+};
 
 export default routes;
